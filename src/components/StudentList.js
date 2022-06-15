02a259be0b0685745185db7e92595a55d3b6cbe7
Esther Annorzie <estherannorzie@gmail.com>
2022-06-15 18:59:27 -0400

    Added student list function and export line

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,2 +1,10 @@
 // Importing React makes the built-in React functionality available through an object named React.
 import React from 'react';
+
+// components begin with a function. Can be defined as arrow functions or regular functions
+const StudentList = () => {
+  return <h2>Student List</h2>;
+};
+
+// to use the StudentList component in other files, export it
+export default StudentList;
