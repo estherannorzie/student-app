7562d5467847456447529d60bda4b85538c0bbfe
Esther Annorzie <estherannorzie@gmail.com>
2022-06-15 20:47:04 -0400

    Unordered list added to StudentList, red borders added to all elements

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,9 +1,19 @@
 // Importing React makes the built-in React functionality available through an object named React.
 import React from 'react';
+import './StudentList.css';
 
 // components begin with a function. Can be defined as arrow functions or regular functions
 const StudentList = () => {
-  return <h2>Student List</h2>;
+  return (
+    <section>
+      <h2>Student List</h2>
+      <ul>
+        <li>Student A</li>
+        <li>Student B</li>
+        <li>Student C</li>
+      </ul>
+    </section>
+  );
 };
 
 // to use the StudentList component in other files, export it
