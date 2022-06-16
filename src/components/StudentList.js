0b0eac61e4018b4a0aec20e6ffcbbdd93955364a
Esther Annorzie <estherannorzie@gmail.com>
2022-06-16 00:53:06 -0400

    Student list held in a variable

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -7,14 +7,17 @@ import Student from './Student';
 const StudentList = () => {
   const headingClass = 'student-list__heading';
   const listClass = 'student-list';
+  const studentComponents = [
+    <li><Student name="Ada" email="ada@dev.org"></Student></li>,
+    <li><Student name="Soo-ah" email="sooah@dev.org"></Student></li>,
+    <li><Student name="Chrissy" email="chrissy@dev.org"></Student></li>
+  ];
 
   return (
     <section>
       <h2 className={headingClass}>Student List</h2>
       <ul className={listClass}>
-        <li><Student name="Ada" email="ada@dev.org"></Student></li>
-        <li><Student name="Soo-ah" email="sooah@dev.org"></Student></li>
-        <li><Student name="Chrissy" email="chrissy@dev.org"></Student></li>
+        {studentComponents}
       </ul>
     </section>
   );
