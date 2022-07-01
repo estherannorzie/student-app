fcdc78c020be522fe76a072efd52a2f59166b4c6
Esther Annorzie <estherannorzie@gmail.com>
2022-07-01 11:56:58 -0400

    Move studentData up to App, give StudentList props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 // StudentList and ClassInfo are imported alongside their relative paths
-import StudentList from './components/StudentList';
-import ClassInfo from './components/ClassInfo';
-
+import StudentList from "./components/StudentList";
+import ClassInfo from "./components/ClassInfo";
+// moving studentData here allows here to display student infor for multiple classes
 function App() {
+  const studentData = [
+    {
+      nameData: "Ada",
+      emailData: "ada@dev.org",
+    },
+    {
+      nameData: "Soo-ah",
+      emailData: "sooah@dev.org",
+    },
+    {
+      nameData: "Chrissy",
+      emailData: "chrissy@dev.org",
+    },
+  ];
+
   return (
     <main>
       <h1>Attendance</h1>
       <ClassInfo></ClassInfo>
-      <StudentList></StudentList>
+      <StudentList students={studentData}></StudentList>
     </main>
   );
 }
--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,41 +1,26 @@
 // Importing React makes the built-in React functionality available through an object named React.
-import React from 'react';
-import './StudentList.css';
-import Student from './Student';
+import React from "react";
+import "./StudentList.css";
+import Student from "./Student";
 
 // components begin with a function. Can be defined as arrow functions or regular functions
-const StudentList = () => {
-  const headingClass = 'student-list__heading';
-  const listClass = 'student-list';
-  const studentData = [
-    {
-      nameData: 'Ada',
-      emailData: 'ada@dev.org'
-    },
-    {
-      nameData: 'Soo-ah',
-      emailData: 'sooah@dev.org'
-    },
-    {
-      nameData: 'Chrissy',
-      emailData: 'chrissy@dev.org'
-    }
-  ];
+const StudentList = (props) => {
+  const headingClass = "student-list__heading";
+  const listClass = "student-list";
 
-  // sets studentComponents to an array of JSX elements. It does this by iterating over each object in studentData, passing each value, one at a time, into our anonymous function as the student parameter. 
-  const studentComponents = studentData.map(student => {
+  // sets studentComponents to an array of JSX elements. It does this by iterating over each object in studentData, passing each value, one at a time, into our anonymous function as the student parameter.
+  const studentComponents = props.students.map((student) => {
     return (
-      <li><Student name={student.nameData} email={student.emailData}></Student></li>
-      );
-    }
-  );
+      <li>
+        <Student name={student.nameData} email={student.emailData}></Student>
+      </li>
+    );
+  });
 
   return (
     <section>
       <h2 className={headingClass}>Student List</h2>
-      <ul className={listClass}>
-        {studentComponents}
-      </ul>
+      <ul className={listClass}>{studentComponents}</ul>
     </section>
   );
 };
