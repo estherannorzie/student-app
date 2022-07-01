61c8a15ac6e3c1ca48d3eeee6f86b5a30bd47654
Esther Annorzie <estherannorzie@gmail.com>
2022-07-01 12:03:36 -0400

    Display the number of students correctly

--- src/components/ClassInfo.js
+++ src/components/ClassInfo.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const ClassInfo = () => {
+const ClassInfo = (props) => {
   return (
     // wrap this content with an outer <section> element, else there will be an error.
     <section>
       <h2>Class Information</h2>
       <ul>
         <li>Name: Team Semicolons</li>
-        <li>Number of members: 30</li>
+        <li>Number of members: {props.numberOfStudents}</li>
       </ul>
     </section>
   );
-}
+};
 
 export default ClassInfo;
