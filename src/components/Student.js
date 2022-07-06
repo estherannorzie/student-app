6c63995a2935c0b5da4e3444825288c2f031d560
Esther Annorzie <estherannorzie@gmail.com>
2022-07-06 12:16:47 -0400

    Remove state

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { useState } from "react";
 import PropTypes from "prop-types";
 import "./Student.css";
 
 // props from StudentList
 const Student = (props) => {
-  const [isPresent, setIsPresent] = useState(false);
-
-  const togglePresence = () => {
-    setIsPresent(!isPresent);
-  };
-
   const nameColor = isPresent ? "green" : "red";
 
   return (
@@ -19,9 +12,7 @@ const Student = (props) => {
         <li className={nameColor}>Nickname: {props.name}</li>
         <li>Email: {props.email}</li>
       </ul>
-      <button onClick={togglePresence}>
-        Toggle if {props.name} is present
-      </button>
+      <button>Toggle if {props.name} is present</button>
     </div>
   );
 };
