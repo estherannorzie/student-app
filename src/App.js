42fc4f39398d7a2c6e07699d5184f1815f09332a
Esther Annorzie <estherannorzie@gmail.com>
2022-07-06 12:02:03 -0400

    Add student presence and id to student data

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import ClassInfo from "./components/ClassInfo";
 function App() {
   const studentData = [
     {
+      id: 1,
       nameData: "Ada",
       emailData: "ada@dev.org",
+      isPresentData: false,
     },
     {
+      id: 2,
       nameData: "Soo-ah",
       emailData: "sooah@dev.org",
+      isPresentData: false,
     },
     {
+      id: 3,
       nameData: "Chrissy",
       emailData: "chrissy@dev.org",
+      isPresentData: false,
     },
   ];
 
