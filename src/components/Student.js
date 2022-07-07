cbad193577476c6664c18a94f47fe451027268fc
Esther Annorzie <estherannorzie@gmail.com>
2022-07-07 10:53:34 -0400

    Update state data when button is clicked

--- src/components/Student.js
+++ src/components/Student.js
@@ -4,7 +4,19 @@ import "./Student.css";
 
 // props from StudentList
 const Student = (props) => {
-  const nameColor = isPresent ? "green" : "red";
+  const onAttendanceButtonClick = () => {
+    const updatedStudent = {
+      id: props.id,
+      nameData: props.name,
+      emailData: props.email,
+      isPresentData: !props.isPresent,
+    };
+    // Invoke the function passed in through the prop named "onUpdate"
+    // This function is referenced by the name "updateStudentData" in App
+    props.onUpdate(updatedStudent);
+  };
+
+  const nameColor = props.isPresent ? "green" : "red";
 
   return (
     <div>
@@ -12,7 +24,9 @@ const Student = (props) => {
         <li className={nameColor}>Nickname: {props.name}</li>
         <li>Email: {props.email}</li>
       </ul>
-      <button>Toggle if {props.name} is present</button>
+      <button onClick={onAttendanceButtonClick}>
+        Toggle if {props.name} is present
+      </button>
     </div>
   );
 };
